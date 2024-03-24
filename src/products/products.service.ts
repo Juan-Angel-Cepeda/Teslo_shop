f6eee71af3296b0c44d72ce5be41ff3d4a55eb8e
angelcepeda <jangelcepeda@protonmail.com>
2024-03-24 13:02:35 -0600

    update in product service, post working and added method HandeDBExceptions

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,19 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductsService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  
+  
+  
+  private readonly logger = new Logger('ProductsService');
+  
+  constructor(
+    
+    //patron repositorio
+    //El patron repositorio inyecta un repositorio de tipo Proudcto para
+    //el manejo de las solicitudes a la base de datos
+    @InjectRepository(Product)
+    private readonly productRepository: Repository<Product>,
+  
+  ) {}
+  
+  async create(createProductDto: CreateProductDto) {
+    try {
+      
+      const product = this.productRepository.create(createProductDto)
+      await this.productRepository.save(product);
+
+      return product;
+
+    }catch(error){
+      this.handleDBExceptions(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all products`;
+  async findAll() {
+    return await this.productRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id:string) {
+    const product = await this.productRepository.findOneBy({id})
+    return product
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
@@ -22,5 +50,13 @@ export class ProductsService {
 
   remove(id: number) {
     return `This action removes a #${id} product`;
+  }
+
+  private handleDBExceptions( error:any){
+    if (error.code === '23505'){
+      throw new BadRequestException(error.detail)
+    }
+    this.logger.error(error);
+    throw new InternalServerErrorException('Unexpected error, check server logs')
   }
 }
