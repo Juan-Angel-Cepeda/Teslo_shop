a777b1c9110c3cf119cfc1974a835eb5759f89fa
angelcepeda <jangelcepeda@protonmail.com>
2024-03-23 20:12:35 -0600

    Product DTO ready by now

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,37 @@
-export class CreateProductDto {}
+import { MinLength, IsString, IsNumber, 
+         IsPositive, IsOptional, IsInt, 
+         IsArray, IsIn 
+        } from "class-validator";
+
+export class CreateProductDto {
+
+    @IsString()
+    @MinLength(1)
+    title:string; 
+
+    @IsNumber()
+    @IsPositive()
+    @IsOptional()
+    price?:number;
+
+    @IsString()
+    @IsOptional()
+    description?:string;
+
+    
+    @IsString()
+    @IsOptional()
+    slug?:string;
+
+    @IsInt()
+    @IsOptional()
+    @IsPositive()
+    stock?:number;
+
+    @IsString({each:true})
+    @IsArray()
+    sizes: string[];
+
+    @IsIn(['men','women','kid','unisex'])
+    gender: string;
+}
