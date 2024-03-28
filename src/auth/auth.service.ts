049c152a023f56116e9e77ad6be205f7bbd403b8
angelcepeda <jangelcepeda@protonmail.com>
2024-03-28 15:12:34 -0600

    Crypt password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,46 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 
+import * as bcrypt from 'bcrypt';
+import { Repository } from 'typeorm';
+
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  create(createUserDto: CreateUserDto) {
-    return 'User created';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>
+
+  ){}
+
+  async create(createUserDto: CreateUserDto) {
+    try{
+
+      const {password, ...userData} = createUserDto;
+      const user = this.userRepository.create({
+        ...userData,
+        password:bcrypt.hashSync(password,10)
+      });
+      
+      await this.userRepository.save(user);
+      delete user.password;
+      const {email,fullName,...rest} = {...user};
+      return {email,fullName};
+      //TODO: Retornar el JWT de acceso
+
+    }catch(error){
+      this.handeDBErrors(error);
+    }
   }
+
+  private handeDBErrors(error: any): never {
+    if(error.code === '23050'){
+      throw new BadRequestException(error.detail)
+    }
+    console.log(error);
+    throw new InternalServerErrorException('Pleas check server logs');
+  }
+
 }
