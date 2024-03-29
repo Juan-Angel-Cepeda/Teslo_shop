92601f4ddc5e2ee2c271a50aa5272ffba2badc45
angelcepeda <jangelcepeda@protonmail.com>
2024-03-28 20:35:10 -0600

    JWT generated ai

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,21 @@
-import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { JwtService } from '@nestjs/jwt';
 
 import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
 
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
-    private readonly userRepository: Repository<User>
+    private readonly userRepository: Repository<User>,
+    private readonly jwtService:JwtService,
 
   ){}
 
@@ -26,13 +30,44 @@ export class AuthService {
       
       await this.userRepository.save(user);
       delete user.password;
-      const {email,fullName,...rest} = {...user};
-      return {email,fullName};
+      
+      return {
+        ...user,
+        token:this.getJwtToken({email:user.email})
+      }
       //TODO: Retornar el JWT de acceso
 
     }catch(error){
       this.handeDBErrors(error);
     }
+  }
+
+  async login(loginUserDto:LoginUserDto){
+    //Data del usuairo tratando de ingresar
+    const { password, email } = loginUserDto;
+    const dbUser = await this.userRepository.findOne({
+      where:{email},
+      select:{email:true,password:true}
+    });
+
+    if(!dbUser){
+      throw new UnauthorizedException("User not found");
+    }
+    
+    if(!bcrypt.compareSync(password,dbUser.password)){
+      throw new UnauthorizedException("Credentials not valid");
+    }
+    
+    return {
+      ...dbUser,
+      token:this.getJwtToken({email:dbUser.email})
+    };
+  }
+
+  private getJwtToken(payload:JwtPayload){
+    //Generacion de Token
+    const token = this.jwtService.sign( payload );
+    return token;
   }
 
   private handeDBErrors(error: any): never {
@@ -42,5 +77,4 @@ export class AuthService {
     console.log(error);
     throw new InternalServerErrorException('Pleas check server logs');
   }
-
 }
