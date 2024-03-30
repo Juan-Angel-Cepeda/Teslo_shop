1b09f2523032d5873ad530aff398f8e71513841a
angelcepeda <jangelcepeda@protonmail.com>
2024-03-30 12:21:19 -0600

    checkAuthStatus func in auth service, checks if user is loged in and generate a new JWT

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -4,6 +4,7 @@ import { initialData } from './data/seed-data';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../auth/entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class SeedService {
@@ -39,7 +40,11 @@ export class SeedService {
     const users:User[] = [];
 
     seedUsers.forEach(user => {
-      users.push(this.userRepository.create(user))
+      const {password, ...userData} = user;
+      users.push(this.userRepository.create({
+        ...userData,
+        password:bcrypt.hashSync(password,10)
+      }));
     });
 
     await this.userRepository.save(users);
